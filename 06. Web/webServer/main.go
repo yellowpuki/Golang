67c78f20e152cacca228391232aea25b0a7867f2
Pavel Timokhovich <yellowpukipost@gmail.com>
2023-04-05 21:48:15 +0300

    refactor

--- 06. Web/webServer/main.go
+++ 06. Web/webServer/main.go
@@ -6,11 +6,22 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<b>Hello, world!</b>\n"))
+	w.Write([]byte("<h1>HOME PAGE</h1>\n"))
+}
+
+func HandlerStudents(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>STUDENTS</h1>\n"))
+}
+
+func HandlerCourses(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>FRONT + BACK</h1>\n"))
 }
 
 func main() {
 	http.HandleFunc("/", Handler)
+	http.HandleFunc("/students", HandlerStudents)
+	http.HandleFunc("/courses", HandlerCourses)
+
 	log.Println("Start HTTP server on port 8081")
 	log.Fatal(http.ListenAndServe(":8081", nil))
 }
