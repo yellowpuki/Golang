9a36d1b8d144288541772f347e80ed66d2f6d693
Pavel Timokhovich <yellowpukipost@gmail.com>
2023-04-10 20:57:54 +0300

    update test

--- test/TestProject/main.go
+++ test/TestProject/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"math"
+	"strings"
 )
 
 type Shape interface {
@@ -35,13 +36,68 @@ func main() {
 		a := 101.0 / 2.0
 		fmt.Print(a + float64(uint8(a)))
 	*/
-	a := "12"
-	//c := 100
-	b := []byte(a)
-	a += string(b)
-	fmt.Println(a)
+	// a := "12"
+	// //c := 100
+	// b := []byte(a)
+	// a += string(b)
+	// fmt.Println(a)
+
+	s := "How can mirrors be real if our eyes aren't real"
+	fmt.Println(Rewerse(s))
+}
+
+func Rewerse(str string) string {
+	word := ""
+	res := ""
+	for _, ch := range str {
+		if ch == ' ' {
+			res += word + " "
+			word = ""
+		} else {
+			word = string(ch) + word
+		}
+	}
+	return res + word
+}
+
+func isPowerOfThree(n int) bool {
+	const (
+		BASE = 3
+		POW  = 19
+	)
+
+	return n > 0 && int(math.Pow(BASE, POW))%n == 0
 }
 
 func printShapeArea(s Shape) {
 	fmt.Printf("Площадь фигуры: %.2f см\n", s.Area())
+}
+
+func ReverseWords(str string) string {
+	var rev string
+	var word string
+
+	for _, i := range str {
+		if i == ' ' {
+			rev = rev + word + " " // Adds word and space to result
+			word = ""              // Empties word variable
+		} else {
+			word = string(i) + word // Adds letter to temporary word variable
+		}
+	}
+
+	return rev + word // reverse those words
+}
+func reverse(r []rune) []rune {
+	for i, j := 0, len(r)-1; i < j; {
+		r[i], r[j] = r[j], r[i]
+		i++
+		j--
+	}
+	return r
+}
+
+func ToJadenCase(str string) string {
+
+	return strings.Title(str) // your code here...
 }
