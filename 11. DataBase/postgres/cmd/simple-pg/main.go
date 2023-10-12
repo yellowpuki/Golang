9a6354819cc2c3357dfdfbfc8d25c58d58334bb5
Pavel Timokhovich <yellowpukipost@gmail.com>
2023-10-12 19:25:28 +0300

    init: add logger

--- 11. DataBase/postgres/cmd/simple-pg/main.go
+++ 11. DataBase/postgres/cmd/simple-pg/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"os"
+
 	"github.com/yellowpuki/simple-pg/internal/config"
+	"golang.org/x/exp/slog"
 )
 
 const (
@@ -16,7 +19,25 @@ func main() {
 	cfg := config.MustLoad()
 
 	// TODO: Logger
+	log := setupLogger(cfg.Env)
+	log.Info("Starting application.", slog.String("env", cfg.Env))
+
 	// TODO: Storage
 	// TODO: Router
 	// TODO: Server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
